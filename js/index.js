4554e4da1a9986f72ed942a2f358d0c72d61b8e1
Efstathios <modo575@hotmail.com>
2025-10-10 13:02:48 +0200

    idk

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 const body = document.body;
 const darkmode = document.querySelector('[data-js="darkmode"]');
+const cards = document.querySelectorAll('[data-js="card"]');
+
+function toggleDarkmode(enable) {
+  cards.forEach((card) => {
+    card.classList.toggle("dark", enable);
+  });
+}
+
+const isDark = localStorage.getItem("darkmode") === "1";
+body.classList.toggle("dark", isDark);
+darkmode.value = isDark ? "1" : "0";
+toggleDarkmode(isDark);
 
 darkmode.addEventListener("input", () => {
-  if (darkmode === "1") {
-    body.classList.add("dark");
-  } else {
-    body.classList.remove("dark");
-  }
+  const enable = darkmode.value === "1";
+  body.classList.toggle("dark", enable);
+  toggleDarkmode(enable);
+  localStorage.setItem("darkmode", enable ? "1" : "0");
 });
